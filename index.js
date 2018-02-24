ee9012111c78be20c43c0c26a3c3cdf6a3f5e4a3
Nicole Eidi <nicoleeidi1@gmail.com>
2018-02-24 17:36:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,26 @@ class Driver {
   }
 }
 class Route {
+  constructor(beginningLocation,endingLocation){
+    this.beginningLocation=beginningLocation
+    this.endingLocation=endingLocation
+  }
+  avenueToInteger(avenue){
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue']
+    return eastWest.indexOf(avenue)
+  }
+  blocksTravelled(){
+
+
+
+    let distance1= Math.abs(this.beginningLocation.horizontal - this.endingLocation.horizontal)
+    let distance2= Math.abs(this.beginningLocation.vertical - this.endingLocation.vertical)
+    return distance1 + distance2
+  }
+  estimatedTime(peak){
+    if (peak){return this.blocksTravelled()/2}
+    else {return this.blocksTravelled()/3}
+  }
+
 
 }
